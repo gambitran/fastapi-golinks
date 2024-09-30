4ddfc833b3c47abec67520b7c60a866820980e25
gambitran <tanngoctran1@gmail.com>
2024-09-30 01:39:17 -0700

    local env and docker-compose

--- app/config.py
+++ app/config.py
@@ -1,14 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv()
+# # Uncomment if local dev
+# from dotenv import load_dotenv
+# load_dotenv()
 
 DB_USERNAME = getenv('DB_USERNAME')
 DB_PASSWORD = getenv('DB_PASSWORD')
-DB_URL = getenv('DB_URL')
-DB_NAME = getenv('DB_NAME')
+DB_URL      = getenv('DB_URL')
+DB_NAME     = getenv('DB_NAME')
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@{DB_URL}/{DB_NAME}'
 
